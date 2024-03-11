6a3d425f1eb44f2a2b44d63379a0eff6236f1e04
J-Potes-GLB <juan.potes@globant.com>
2024-03-11 03:52:05 -0500

    Added the step definitions in ClientSteps

--- src/test/java/com/jp/api/stepDefinitions/ClientSteps.java
+++ src/test/java/com/jp/api/stepDefinitions/ClientSteps.java
@@ -1,4 +1,40 @@
 package com.jp.api.stepDefinitions;
 
+import com.jp.api.models.Client;
+import com.jp.api.requests.ClientRequest;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ClientSteps {
+    private static final Logger logger = LogManager.getLogger(ClientSteps.class);
+
+    private final ClientRequest clientRequest = new ClientRequest();
+
+    private Response response;
+    private Client client;
+
+    @Given("there are at least {int} registered clients on the system")
+    public void thereAreRegisteredClientsOnTheSystem(int numberOfClientsRegistered){
+        logger.info("there are at least " + numberOfClientsRegistered + " registered clients in the system");
+    }
+
+    @When("I send a GET request to view all the clients")
+    public void iSendAGETRequestToViewAllTheClients() {
+        logger.info("I send a GET request to view all the clients");
+    }
+
+    @Then("the response should have a status code of {int}")
+    public void theResponseShouldHaveAStatusCodeOf(int expectedStatusCode) {
+        logger.info("the response should have a status code of " + expectedStatusCode);
+    }
+
+    @And("validates the response with client list JSON schema")
+    public void validatesTheResponseWithClientListJSONSchema() {
+        logger.info("validates the response with client list JSON schema");
+    }
 }
--- src/test/resources/features/client_testing.feature
+++ src/test/resources/features/client_testing.feature
@@ -1,8 +1,9 @@
 @active
 Feature: Client testing CRUD
 
+  @smoke
   Scenario: Get the list of clients
-    Given there are registered clients on the system 3
+    Given there are at least 3 registered clients on the system
     When I send a GET request to view all the clients
     Then the response should have a status code of 200
     And validates the response with client list JSON schema
